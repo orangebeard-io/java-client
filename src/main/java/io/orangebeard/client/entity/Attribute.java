8fed04da09f1fb219b0bf8da571b23f105756e1b
Maarten-Jan van Gool <maarten-jan.van.gool@praegus.nl>
2020-07-28 09:29:15 +0200

    [SOJU-535] fix inlezen properties

--- src/main/java/io/orangebeard/client/entity/Attribute.java
+++ src/main/java/io/orangebeard/client/entity/Attribute.java
@@ -1,11 +1,11 @@
 package io.orangebeard.client.entity;
 
-import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-@AllArgsConstructor
+import static java.lang.String.format;
+
 @NoArgsConstructor
 @Getter
 @EqualsAndHashCode
@@ -13,8 +13,24 @@ public class Attribute {
     private String key;
     private String value;
 
+    /**
+     * New attribute. Colons and Semicolons are removed because these are special values.
+     */
+    public Attribute(String key, String value) {
+        this.key = key.replace(":", "").replace(";", "").trim();
+        this.value = value.replace(":", "").replace(";", "").trim();
+    }
+
+    /**
+     * New attribute. Colons and Semicolons are removed because these are special values.
+     */
     public Attribute(String value) {
-        this.value = value;
+        this.value = value.replace(":", "").replace(";", "").trim();
+    }
+
+    @Override
+    public String toString() {
+        return format("key: %s; value:%s", key, value);
     }
 }
 
