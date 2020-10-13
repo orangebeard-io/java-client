83cc01b9d2c7d94243939e1f18805ec21af089af
Maarten-Jan van Gool <maarten-jan.van.gool@praegus.nl>
2020-10-13 17:10:40 +0200

    support for version 2 of the api

--- src/test/java/io/orangebeard/client/OrangebeardV2ClientTest.java
+++ src/test/java/io/orangebeard/client/OrangebeardV2ClientTest.java
@@ -20,35 +20,35 @@ import static org.mockito.Mockito.when;
 import static org.springframework.http.HttpMethod.POST;
 
 @ExtendWith(MockitoExtension.class)
-class OrangebeardClientTest {
+class OrangebeardV2ClientTest {
 
     @Mock
     private RestTemplate restTemplate;
 
     @Test
     public void when_the_connection_is_valid_a_log_is_reported_to_orangebeard(){
-        OrangebeardClient orangebeardClient = new OrangebeardClient(restTemplate, "http://localhost:8080", UUID.fromString("92580f91-073a-4bf7-aa10-bb4f8dbcb534"), "project", true);
+        OrangebeardV2Client orangebeardClient = new OrangebeardV2Client(restTemplate, "http://localhost:8080", UUID.fromString("92580f91-073a-4bf7-aa10-bb4f8dbcb534"), "project", true);
 
         orangebeardClient.log(Log.builder().build());
 
-        verify(restTemplate, times(1)).exchange(eq("http://localhost:8080/listener/v1/project/log"), eq(POST), any(), eq(Response.class));
+        verify(restTemplate, times(1)).exchange(eq("http://localhost:8080/listener/v2/project/log"), eq(POST), any(), eq(Response.class));
 
     }
 
     @Test
     public void when_the_connection_is_invalid_no_log_is_reported_to_orangebeard(){
-        OrangebeardClient orangebeardClient = new OrangebeardClient(restTemplate, "http://localhost:8080", UUID.fromString("92580f91-073a-4bf7-aa10-bb4f8dbcb534"), "project", false);
+        OrangebeardV2Client orangebeardClient = new OrangebeardV2Client(restTemplate, "http://localhost:8080", UUID.fromString("92580f91-073a-4bf7-aa10-bb4f8dbcb534"), "project", false);
 
         orangebeardClient.log(Log.builder().build());
 
-        verify(restTemplate, times(0)).exchange(eq("http://localhost:8080/listener/v1/project/log"), eq(POST), any(), eq(Response.class));
+        verify(restTemplate, times(0)).exchange(eq("http://localhost:8080/listener/v2/project/log"), eq(POST), any(), eq(Response.class));
     }
 
     @Test
     public void when_the_connection_throws_a_timeout_exception_this_is_properly_handled(){
-        when(restTemplate.exchange(eq("http://localhost:8080/listener/v1/project/log"), eq(POST), any(), eq(Response.class))).thenThrow(new ResourceAccessException("error!"));
+        when(restTemplate.exchange(eq("http://localhost:8080/listener/v2/project/log"), eq(POST), any(), eq(Response.class))).thenThrow(new ResourceAccessException("error!"));
 
-        OrangebeardClient orangebeardClient = new OrangebeardClient(restTemplate, "http://localhost:8080", UUID.fromString("92580f91-073a-4bf7-aa10-bb4f8dbcb534"), "project", true);
+        OrangebeardV2Client orangebeardClient = new OrangebeardV2Client(restTemplate, "http://localhost:8080", UUID.fromString("92580f91-073a-4bf7-aa10-bb4f8dbcb534"), "project", true);
 
         assertThatCode(() -> orangebeardClient.log(Log.builder().build())).doesNotThrowAnyException();
 
