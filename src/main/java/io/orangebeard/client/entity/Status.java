b33d1a1906ed530a7410198a8ab67af120184cda
Maarten-Jan van Gool <maarten-jan.van.gool@praegus.nl>
2020-10-30 13:15:24 +0100

    added the interrupted status

--- src/main/java/io/orangebeard/client/entity/Status.java
+++ src/main/java/io/orangebeard/client/entity/Status.java
@@ -1,5 +1,12 @@
 package io.orangebeard.client.entity;
 
 public enum Status {
-    PASSED, FAILED, STOPPED, SKIPPED, RESETED, CANCELLED
+    IN_PROGRESS,
+    PASSED,
+    FAILED,
+    STOPPED, //status for manually stopped launches
+    SKIPPED,
+    INTERRUPTED,
+    RESETED, //status for items with deleted descendants
+    CANCELLED
 }
