a563cec81b5b0858dde150939919b4303caa7c24
Maarten-Jan van Gool <maarten-jan@orangebeard.io>
2021-04-07 14:53:55 +0200

    [SOJU-882] fix so milliseconds are marshalled as well

--- src/main/java/io/orangebeard/client/entity/DateSerializer.java
+++ src/main/java/io/orangebeard/client/entity/DateSerializer.java
@@ -1,16 +1,26 @@
 package io.orangebeard.client.entity;
 
-import java.io.IOException;
-import java.time.LocalDateTime;
-import java.time.ZoneId;
 import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.databind.JsonSerializer;
 import com.fasterxml.jackson.databind.SerializerProvider;
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
 
 class DateSerializer extends JsonSerializer<LocalDateTime> {
 
+    private final ZoneId zoneId;
+
+    public DateSerializer() {
+        this.zoneId = ZoneId.systemDefault();
+    }
+
+    public DateSerializer(ZoneId zoneId) {
+        this.zoneId = zoneId;
+    }
+
     @Override
     public void serialize(LocalDateTime value, JsonGenerator jgen, SerializerProvider serializers) throws IOException {
-        jgen.writeRawValue(String.valueOf(value.atZone(ZoneId.systemDefault()).toInstant().toEpochMilli()));
+        jgen.writeRawValue(String.valueOf(value.atZone(zoneId).toInstant().toEpochMilli()));
     }
 }
--- src/test/java/io/orangebeard/client/entity/DateSerializerTest.java
+++ src/test/java/io/orangebeard/client/entity/DateSerializerTest.java
@@ -2,6 +2,7 @@ package io.orangebeard.client.entity;
 
 import java.io.IOException;
 import java.time.LocalDateTime;
+import java.time.ZoneId;
 import com.fasterxml.jackson.core.JsonGenerator;
 import org.junit.jupiter.api.Test;
 
@@ -13,12 +14,12 @@ class DateSerializerTest {
 
     @Test
     public void test() throws IOException {
-        DateSerializer dateSerializer = new DateSerializer();
+        DateSerializer dateSerializer = new DateSerializer(ZoneId.of("UTC"));
 
         JsonGenerator jsonGenerator = mock(JsonGenerator.class);
         dateSerializer.serialize(LocalDateTime.parse("2021-04-07T14:30:53.314089"), jsonGenerator, null);
 
-        verify(jsonGenerator).writeRawValue(eq("1617798653314"));
+        verify(jsonGenerator).writeRawValue(eq("1617805853314"));
     }
 
 }
