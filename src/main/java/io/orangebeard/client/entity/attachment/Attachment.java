004e532f6a5ac47b0e798e74fedae1dc62add379
tcnh <tom@orangebeard.io>
2023-03-17 11:17:49 +0100

    [team-soju#4161]Basic v3 impl

---
+++ src/main/java/io/orangebeard/client/entity/attachment/Attachment.java
@@ -0,0 +1,38 @@
+package io.orangebeard.client.entity.attachment;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import java.time.ZonedDateTime;
+import java.util.UUID;
+
+@AllArgsConstructor
+@Getter
+public class Attachment {
+
+    private AttachmentFile file;
+    private AttachmentMetaData metaData;
+
+    @AllArgsConstructor
+    @Getter
+    @NoArgsConstructor
+    @Builder
+    public class AttachmentFile {
+        private String name;
+        private byte[] content;
+        private String contentType;
+    }
+
+    @AllArgsConstructor
+    @Getter
+    @NoArgsConstructor
+    @Builder
+    public class AttachmentMetaData {
+        private UUID testRunUUID;
+        private UUID testUUID;
+        private UUID stepUUID;
+        private UUID logUUID;
+        private ZonedDateTime attachmentTime;
+    }
+}
---
+++ src/main/java/io/orangebeard/client/entity/log/Log.java
@@ -0,0 +1,27 @@
+package io.orangebeard.client.entity.log;
+
+import io.orangebeard.client.entity.LogFormat;
+import io.orangebeard.client.entity.LogLevel;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import java.time.ZonedDateTime;
+import java.util.UUID;
+
+@AllArgsConstructor
+@Builder
+@Getter
+@EqualsAndHashCode
+@NoArgsConstructor
+public class Log {
+    private UUID testRunUUID;
+    private UUID testUUID;
+    private UUID stepUUID;
+    private String message;
+    private LogLevel logLevel;
+    private ZonedDateTime logTime;
+    private LogFormat logFormat;
+}
