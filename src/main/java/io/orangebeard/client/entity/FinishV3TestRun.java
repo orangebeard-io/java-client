0c3d41a7500c65be93c02bfc93a62b5d08e121fd
patriksimixhiu <patrik@orangebeard.io>
2023-04-29 12:52:21 +0200

    [team-soju#4161] FinishV3TestRun updated.

--- src/main/java/io/orangebeard/client/entity/FinishV3TestRun.java
+++ src/main/java/io/orangebeard/client/entity/FinishV3TestRun.java
@@ -1,13 +1,11 @@
 package io.orangebeard.client.entity;
 
-import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import lombok.Getter;
 import java.time.ZonedDateTime;
 
 @Getter
 public class FinishV3TestRun {
 
-    @JsonSerialize(using = DateSerializer.class)
     private final ZonedDateTime endTime;
 
     public FinishV3TestRun() {
