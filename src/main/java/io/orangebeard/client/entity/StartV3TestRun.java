15e218b7caf5d430c74fe32a5033ab331a93eb3e
tcnh <tom@orangebeard.io>
2023-05-01 17:04:34 +0200

    [team-soju#4161] Use the right datetime

--- src/main/java/io/orangebeard/client/entity/StartV3TestRun.java
+++ src/main/java/io/orangebeard/client/entity/StartV3TestRun.java
@@ -1,5 +1,7 @@
 package io.orangebeard.client.entity;
 
+import java.time.ZonedDateTime;
+import java.util.Set;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import lombok.AllArgsConstructor;
@@ -7,8 +9,6 @@ import lombok.Builder;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import java.time.LocalDateTime;
-import java.util.Set;
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @Getter
@@ -21,21 +21,21 @@ public class StartV3TestRun {
     private String testSetName;
     private String description;
     @JsonSerialize(using = DateSerializer.class)
-    private LocalDateTime startTime;
+    private ZonedDateTime startTime;
     private Set<Attribute> attributes;
     private Set<ChangedComponent> changedComponents;
 
     public StartV3TestRun(String testSetName, String description, Set<Attribute> attributes) {
         this.testSetName = testSetName;
         this.description = description;
-        this.startTime = LocalDateTime.now();
+        this.startTime = ZonedDateTime.now();
         this.attributes = attributes;
     }
 
     public StartV3TestRun(String testSetName, String description, Set<Attribute> attributes, Set<ChangedComponent> changedComponents) {
         this.testSetName = testSetName;
         this.description = description;
-        this.startTime = LocalDateTime.now();
+        this.startTime = ZonedDateTime.now();
         this.attributes = attributes;
         this.changedComponents = changedComponents;
     }
